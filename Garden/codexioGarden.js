0247f9391e3e4f5571b68778abca8133b4b9f895
Aleksandar Vulchanov <aleks.vulchanov@abv.bg>
2023-04-20 23:13:17 +0300

    commit

---
+++ Garden/codexioGarden.js
@@ -0,0 +1,45 @@
+function rabbit(garden) {
+    let jumps = 0;
+
+    for (let currRow = 0; currRow < garden.length; currRow++) {
+        let row = garden[currRow];
+        for (let currPosition = 0; currPosition < row.length; currPosition++) {
+            if (row[currPosition] == "x") {
+                jumps += 1;
+                isCarrot(garden, currRow, currPosition);
+            };
+        };
+    };
+
+    return jumps;
+};
+
+function isCarrot(garden, x, y) {
+    if (x < 0 || x >= garden.length) {
+        return;
+    }
+
+    if (y < 0 || y >= garden[x].length) {
+        return;
+    }
+
+    if (garden[x][y] !== "x") {
+        return;
+    }
+
+    garden[x][y] = "eaten";
+
+    isCarrot(garden, x + 1, y);
+    isCarrot(garden, x - 1, y);
+    isCarrot(garden, x, y + 1);
+    isCarrot(garden, x, y - 1);
+}
+
+rabbit([
+    ["x", "x", " ", " ", " "],
+    ["x", "x", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", "x", "x"],
+    [" ", " ", " ", "x", "x"],
+]);
+
---
+++ Matching Letters/codexioMatchingLetters.js
@@ -0,0 +1,27 @@
+function elf(firstWord, secondWord){
+    let str1 = firstWord.split("");
+    let str2 = secondWord.split("");
+
+    let result = 0;
+
+    let [shorterWord, longerWord] = checkWords(firstWord, secondWord);
+
+    for(let char of shorterWord){
+        if(longerWord.includes(char)){
+            let indexOfstr1 = str1.indexOf(char);
+            let indexOfstr2 = str2.indexOf(char);
+
+            str1.splice(indexOfstr1, 1);
+            str2.splice(indexOfstr2, 1);
+        };
+    };
+
+    result = str1.length + str2.length;
+    return result;
+};
+
+function checkWords(a, b) {
+    return a.length <= b.length ? [a, b] : [b, a];
+};
+
+elf('codewars', 'hackerrank');
